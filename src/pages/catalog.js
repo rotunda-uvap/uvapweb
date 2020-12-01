281b7dd1868d7e950814ac1ef29481cc91472c85
Patricia Searl <patriciasearl@d-172-18-100-131.vpn.net.virginia.edu>
2020-12-01 12:33:14 -0500

    catalog update

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -11,13 +11,13 @@ export default function Catalog({data}) {
        <p>Download Current Catalog
          <a href={`/fall20.pdf`}> <img src={`/fall20.jpg`}/></a></p>
        <p>Previous catalog links</p>
-       <ul className="grid grid-cols-2 md:grid-cols-3">
-       <li><a href={'/spring20.pdf'}><StaticImage  width={200} src="../images/spring20.jpg" alt="Spring 2020" /></a><button className="w-full text-white p-2 bg-gray-800">Spring 2020</button></li>
-         <li><a href={'/fall19.pdf'}><StaticImage  width={200} src="../images/fall19.jpg" alt="Fall 2019" /></a><button className="w-full text-white p-2 bg-gray-800">Fall 2019</button></li>
-         <li><a href={'/spring19.pdf'}><StaticImage  width={200} src="../images/spring_19.jpg" alt="Spring 2019" /></a><button className="w-full text-white p-2 bg-gray-800">Spring 2019</button></li>
-         <li><a href={'/fall18.pdf'}><StaticImage  width={200} src="../images/fall18.jpg" alt="Fall 2018" /></a><button className="w-full text-white p-2 bg-gray-800">Fall 2018</button></li>
-         <li><a href={'/spring18.pdf'}><StaticImage  width={200} src="../images/spring_18.jpg" alt="Spring 2018" /></a><button className="w-full text-white p-2 bg-gray-800">Spring 2018</button></li>
-         <li><a href={'/fall17.pdf'}><StaticImage  width={200} src="../images/fall17.jpg" alt="Fall 2017" /></a><button className="w-full text-white p-2 bg-gray-800">Fall 2017</button></li>
+       <ul className="grid grid-cols-2 md:grid-cols-3 gap-y-1">
+       <li><a href={'/spring20.pdf'}><StaticImage  width={149} src="../images/spring20.jpg" alt="Spring 2020" /></a><button className="w-auto ml-2 text-white p-2 bg-gray-800">Spring 2020</button></li>
+         <li><a href={'/fall19.pdf'}><StaticImage  width={149} src="../images/fall19.jpg" alt="Fall 2019" /></a><button className="w-auto  text-white p-2 bg-gray-800">Fall 2019</button></li>
+         <li><a href={'/spring19.pdf'}><StaticImage  width={149} src="../images/spring_19.jpg" alt="Spring 2019" /></a><button className="w-auto text-white p-2 bg-gray-800">Spring 2019</button></li>
+         <li><a href={'/fall18.pdf'}><StaticImage  width={149} src="../images/fall18.jpg" alt="Fall 2018" /></a><button className="w-auto text-white p-2 bg-gray-800">Fall 2018</button></li>
+         <li><a href={'/spring18.pdf'}><StaticImage  width={149} src="../images/spring_18.jpg" alt="Spring 2018" /></a><button className="w-auto text-white p-2 bg-gray-800">Spring 2018</button></li>
+         <li><a href={'/fall17.pdf'}><StaticImage  width={149} src="../images/fall17.jpg" alt="Fall 2017" /></a><button className="w-auto text-white p-2 bg-gray-800">Fall 2017</button></li>
 
          </ul>
        <p>Link to Edelweiss</p>
