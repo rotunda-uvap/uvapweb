d5b8f9c00a21f53eb9d7213f4cde22e3271e9bbc
meadowlarque <patricia.searl@gmail.com>
2020-12-22 10:43:44 -0500

    CMS Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,13 +35,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: path.join(__dirname, `src`, `images`),
       },
