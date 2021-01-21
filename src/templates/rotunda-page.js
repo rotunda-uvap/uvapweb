4a6ae87076a5aa2014c421b4a1355446eb3a020d
meadowlarque <patricia.searl@gmail.com>
2021-01-21 18:29:16 -0500

    Rotunda Content

--- src/templates/rotunda-page.js
+++ src/templates/rotunda-page.js
@@ -4,12 +4,14 @@ import Layout from "../components/layout"
 
 export default ({ data }) => {
     const item = data.rotundaJson
+    const summary = data.markdownRemark
  return (
    <Layout>
      <div>
         <h2 className="py-10">{item.Title}</h2>
         <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
-          
+        <article 
+          dangerouslySetInnerHTML={{ __html:summary.html }}/>
           </div>
         
     </div>
@@ -26,6 +28,18 @@ export const query = graphql`
         RotID
         Title
     }
+    markdownRemark(frontmatter: {templateKey: {eq: "rotunda"}}, id: { eq: $id }) {
+      html
+      id
+      frontmatter {
+          templateKey
+          title
+          related_books {
+            book_id
+            book_title
+          }
+      }
+  }
   }
 `
 
