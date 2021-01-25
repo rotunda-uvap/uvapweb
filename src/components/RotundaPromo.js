68489004004b45299a41db786a34fe19279117aa
meadowlarque <patricia.searl@gmail.com>
2021-01-25 16:45:29 -0500

    updates, mods

--- src/components/RotundaPromo.js
+++ src/components/RotundaPromo.js
@@ -10,6 +10,7 @@ export default function RotundaPromo() {
                 frontmatter {
                   body1
                   body2
+                  rid
                   
                 }
               }
@@ -25,12 +26,12 @@ export default function RotundaPromo() {
         <div className="bg-cover bg-center h-auto text-white py-20 px-10 object-fill grid md:grid-cols-2 gap-4 content-center items-center">
        <div>
         <p className="font-bold text-sm uppercase">Featured Rotunda:</p>
-        <Link to={'../../title/5477'}><p className="text-3xl font-bold">{data.rot.frontmatter.body1}</p></Link>
+        <Link to={'../../title/' + data.rot.frontmatter.rid}><p className="text-3xl font-bold">{data.rot.frontmatter.body1}</p></Link>
 
         <p className="body-text p-5">{data.rot.frontmatter.body2} </p>
         </div>  
         <div>
-        <Link to={'/'}><GatsbyImage image={getImage(data.rotImg)} alt="featured rotunda image" /></Link>
+        <Link to={'../../title/' + data.rot.frontmatter.rid}><GatsbyImage image={getImage(data.rotImg)} alt="featured rotunda image" /></Link>
         
         </div>  
     </div>
