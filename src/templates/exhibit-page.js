755fbe5023c8bf145fed12da1c5d011ce3ddffc5
meadowlarque <patricia.searl@gmail.com>
2021-02-03 19:58:06 -0500

    update

--- src/templates/exhibit-page.js
+++ src/templates/exhibit-page.js
@@ -9,6 +9,8 @@ export default ({ data }) => {
     const related_book_group1 = data.markdownRemark.frontmatter.related_1
     const related_series = data.markdownRemark.frontmatter.related_series
     const related_staff = data.markdownRemark.frontmatter.related_staff
+    const bgcolor = exhibit.frontmatter.bgcolor
+    const txtcolor = exhibit.frontmatter.txtcolor
     const image = getImage(data.Img)
 
  return (
@@ -16,22 +18,57 @@ export default ({ data }) => {
      <div>
         <GatsbyImage image={image} alt="related image"/>
         <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
-          <p className="py-5"
+          <p className={`${bgcolor} ${txtcolor} py-5`}
           dangerouslySetInnerHTML={{ __html:exhibit.html }}/>
-          
+
           <h6 className="py-5">Meet our Editors:</h6>
         {related_staff && related_staff.map(staff => (
             <> 
             <Link to={'../../staff/' + staff.replace(" ", "-").toLowerCase()}> <div>{staff}</div></Link>
             </>
         ))}
-         <h6 className="py-5">Related Books:</h6>
+         {exhibit.frontmatter.bkg1_title &&
+         <section>
+         <h6 className="py-5">{exhibit.frontmatter.bkg1_title}</h6>
           {related_book_group1 && related_book_group1.map(book => (
             <> 
-            <RelatedBook id={book.book_id}/>
+            <RelatedBook id={book}/>
             
             </>
         ))}
+         </section>}
+
+         {exhibit.frontmatter.bkg2_title &&
+         <section>
+         <h6 className="py-5">{exhibit.frontmatter.bkg2_title}</h6>
+          {data.markdownRemark.frontmatter.related_2 && data.markdownRemark.frontmatter.related_2.map(book => (
+            <> 
+            <RelatedBook id={book}/>
+            
+            </>
+        ))}
+         </section>}
+         {exhibit.frontmatter.bkg3_title &&
+         <section>
+         <h6 className="py-5">{exhibit.frontmatter.bkg3_title}</h6>
+          {data.markdownRemark.frontmatter.related_3 && data.markdownRemark.frontmatter.related_3.map(book => (
+            <> 
+            <RelatedBook id={book}/>
+            
+            </>
+        ))}
+         </section>}
+         
+         {exhibit.frontmatter.bkg4_title &&
+         <section>
+         <h6 className="py-5">{exhibit.frontmatter.bkg4_title}</h6>
+          {data.markdownRemark.frontmatter.related_4 && data.markdownRemark.frontmatter.related_4.map(book => (
+            <> 
+            <RelatedBook id={book}/>
+            
+            </>
+        ))}
+         </section>}
         <h6 className="py-5">Related Series:</h6>
         {related_series && related_series.map(series => (
             <> 
@@ -59,8 +96,17 @@ export const query = graphql`
             title
             related_series
             related_staff
+            featured_books
             bkg1_title
+            bkg2_title
+            bkg3_title
+            bkg4_title
             related_1
+            related_2
+            related_3
+            related_4
+            bgcolor
+            txtcolor
         } 
       }
     Img: file(extension: {eq: "jpg"}, relativeDirectory: {eq: $relDir}) {
