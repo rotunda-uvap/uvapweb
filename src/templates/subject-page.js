fa1e3460bc5b1e1221576d392495d4085a78efd8
patricia <pls4e@virginia.edu>
2021-11-19 19:28:02 -0500

    another typo

--- src/templates/subject-page.js
+++ src/templates/subject-page.js
@@ -19,7 +19,7 @@ const SubjectTemplate = ({ data }) => {
   <path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd" />
     </svg>
             <a
-              href={`../../books#subjects`}
+              href={`../../books/#subjects`}
               className="block p-2  text-xs font-thin uppercase "
             >All Subjects</a>
     </div>
