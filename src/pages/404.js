332d827d05513f96174749c98bdec6ac71e2cf84
patricia <pls4e@virginia.edu>
2021-12-03 20:34:04 -0500

    404 page text

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,20 +8,20 @@ const NotFound = () => {
   return (
     <Layout>
       <SeO title="Page not found"/>
-      <div className=" min-h-screen flex items-center text-gray-500">
+      <div className=" min-h-screen flex items-center text-ceci-gray-dark">
   <div className="container mx-auto p-5 flex flex-wrap items-center">
     <div className="w-full md:w-5/12 text-center p-4">
     <StaticImage  src="../images/404.png" alt="page missing" />
     </div>
     <div className="w-full md:w-7/12 text-center md:text-left p-4">
       <div className="text-6xl font-medium">404</div>
-      <div className="text-xl md:text-3xl font-medium mb-4">
-        Oops. This page has gone missing.
+      <div className="text-xl md:text-2xl font-thin tracking-wide text-ceci-gray-mid mb-1">
+        Rats. This page has gone missing.
       </div>
-      <div className="text-lg mb-6">
-        The address may be incorrect or the page may have moved.
+      <div className="text-lg mb-3 font-serif font-thin tracking-wide">
+        It may have been moved or recycled during our recent redesign. Hopefully you will find what you seek via the top menu.  
       </div>
-      <Link to="/" className="border border-gray-500 rounded p-2">Go Home</Link>
+      <Link to="/" className="border border-ceci-gray-mid text-ceci-gray-mid p-2">Go Home</Link>
     </div>
   </div>
 </div>
