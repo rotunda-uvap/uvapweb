07a805abc0c6184991604e0a42301cdb263992d7
patricia <pls4e@virginia.edu>
2022-01-12 10:44:00 -0500

    font edits

--- src/templates/book-page.js
+++ src/templates/book-page.js
@@ -61,14 +61,14 @@ const bookPage = ({ data }) => {
         </div>
         <div className="md:col-span-2 pr-5 md:px-10 text-ceci-gray-dark">
           <h4 className="hidden md:block pb-3 font-thin font-sans leading-none ">{book.Title}</h4>
-          <h4 className="md:hidden pb-3 font-thin leading-relaxed font-sans">{book.Title}</h4>
+          <h4 className="md:hidden pb-3 font-thin leading-relaxed uppercase font-sans">{book.Title}</h4>
 
           {book.InternalSeriesVolume && (
             <h6 className="py-3">{book.InternalSeriesVolume}</h6>
           )}
 
           {book.Subtitle && (
-            <h6 className="italic font-thin text-ceci-gray-mid tracking-wide">{book.Subtitle}</h6>
+            <h6 className="italic font-thin text-ceci-gray-mid">{book.Subtitle}</h6>
           )}
 
           <h6 className="py-2 font-display">
