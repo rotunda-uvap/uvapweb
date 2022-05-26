5a51ba0cb175fe976ebb2655ee3964c4b8f46133
pls4e <31322343+pls4e@users.noreply.github.com>
2022-05-26 16:07:05 -0400

    above the fold images eager

--- src/components/HomepageBanner.js
+++ src/components/HomepageBanner.js
@@ -26,7 +26,7 @@ const uvafullimage = getImage(data.uvalogofull)
     <section className=" w-full md:w-3/4 lg:w-2/3 mx-auto py-5  text-ceci-gray-dark">
    
        <div className="mx-auto grid gap-4 grid-cols-2 lg:grid-cols-3  place-content-center justify-center content-center  ">
-       <div id="1" className="col-span-2 lg:col-span-3 w-full py-5 place-content-center"><div className=" text-center pb-5"><GatsbyImage image={uvafullimage} alt="uva logo"width={500} height={119} className="homepageimage" /></div></div> 
+       <div id="1" className="col-span-2 lg:col-span-3 w-full py-5 place-content-center"><div className=" text-center pb-5"><GatsbyImage image={uvafullimage} alt="uva logo"width={500} height={119} className="homepageimage" loading="eager"/></div></div> 
        <div id="2" className="col-span-1  flex flex-col justify-center"><Link to={booklink}>
          <div className=" font-sans font-light text-xs md:text-lg tracking-wide text-ceci-gray-mid text-right uppercase">{author}</div>
          <div className=" font-display text-xl md:text-3xl font-light text-right tracking-wide uppercase">{book_title}</div>
@@ -34,7 +34,7 @@ const uvafullimage = getImage(data.uvalogofull)
         </Link> </div>
          
         <div id="3" className="col-span-1 flex flex-col justify-center items-center"><div className="self-center">
-        <Link to={booklink}> <img width={200} height={112} src={cover} alt="feat book" className="drop-shadow-img homepageimage" /></Link></div>
+        <Link to={booklink}> <img width={200} height={112} src={cover} alt="feat book" className="drop-shadow-img homepageimage" loading="eager" /></Link></div>
           </div>
         
           <div id="4" className="hidden lg:block lg:col-span-1 py-3 font-serif font-light text-2xl quoted self-center text-left">
