3a805241a16106ef40cf22ce5e84c1bd21b8fe85
pls4e <31322343+pls4e@users.noreply.github.com>
2022-05-27 17:23:08 -0400

    grr

--- src/components/NewsEventEx.js
+++ src/components/NewsEventEx.js
@@ -81,14 +81,14 @@ export default function FeatTrio() {
            
             <>
               <div className="flex flex-row mb-2">
-                  <div className="border-r-2 border-gray-600 uppercase tracking-wider text-ceci-gray-dark font-serif  text-sm md:text-md pr-2 md:pr-3"><Link to={`/publicity`}>News and Events</Link></div>
-                  <div className="tracking-widest text-ceci-gray-mid font-serif text-xs pl-1 md:pl-3   self-center">{data.news.edges[0].node.frontmatter.date}</div>
+                  <Link to={`/publicity`}><div className="border-r-2 border-gray-600 uppercase tracking-wider text-ceci-gray-dark font-serif  text-sm md:text-md pr-2 md:pr-3">News and Events</div>
+                  <div className="tracking-widest text-ceci-gray-mid font-serif text-xs pl-1 md:pl-3   self-center">{data.news.edges[0].node.frontmatter.date}</div></Link>
               </div>
           {/*     {data.news.edges[0].node.frontmatter.related_book ? 
               <div className="flex flex-row"><img src={'https://ik.imagekit.io/uvapress/mediums/' + data.news.edges[0].node.frontmatter.related_book[0].jsonId + "_M.jpg"} alt="cover" className="justify-self-center object-cover w-1/3 p-2"/> 
               <Link className="font-display tracking-wide uppercase py-2" to={`../${ data.news.edges[0].node.frontmatter.type }${ data.news.edges[0].node.fields.slug }`}>{data.news.edges[0].node.frontmatter.title }</Link></div>
                :<Link className="font-display font-thin tracking-wider uppercase py-2" to={`../${ data.news.edges[0].node.frontmatter.type }${ data.news.edges[0].node.fields.slug }`}>{data.news.edges[0].node.frontmatter.title }</Link> } */}
-<Link className="font-display font-thin tracking-wider uppercase py-2" to={`../${ data.news.edges[0].node.frontmatter.type }${ data.news.edges[0].node.fields.slug }`}>{data.news.edges[0].node.frontmatter.title }</Link>
+<Link className="font-display font-thin tracking-wider uppercase p-2" to={`../${ data.news.edges[0].node.frontmatter.type }${ data.news.edges[0].node.fields.slug }`}>{data.news.edges[0].node.frontmatter.title }</Link>
               {/* <Link className="font-display tracking-widest uppercase py-2" to={`../${ data.news.edges[0].node.frontmatter.type }${ data.news.edges[0].node.fields.slug }`}>{data.news.edges[0].node.frontmatter.title }</Link> */}
             {data.news.edges[0].node.html &&  <div className=" font-serif text-md leading-relaxed text-gray-800 dropCap" dangerouslySetInnerHTML={{ __html: data.news.edges[0].node.html.split(' ').splice(0, 20).join(' ') + '...' }}/>}
             <div className="font-display uppercase text-xs tracking-widest"><Link  to={`../${ data.news.edges[0].node.frontmatter.type }${ data.news.edges[0].node.fields.slug }`}><FaArrowAltCircleRight className="inline mx-4"/> Continue Reading</Link> 
@@ -100,8 +100,8 @@ export default function FeatTrio() {
            <>
              <div className="flex flex-row md:px-10 "> 
              
-                 <div className="border-r-2 border-gray-600 uppercase tracking-wider text-ceci-gray-dark font-serif  text-sm md:text-md pr-1 md:pr-3"><Link to={`/authors-corner`}>Author's Corner</Link></div>
-                 <div className="tracking-widest text-ceci-gray-mid font-serif text-xs pl-1 md:pl-3 self-center">{data.ac.edges[0].node.frontmatter.date}</div>
+                 <Link to={`/authors-corner`}><div className="border-r-2 border-gray-600 uppercase tracking-wider text-ceci-gray-dark font-serif  text-sm md:text-md pr-1 md:pr-3">Author's Corner</div>
+                 <div className="tracking-widest text-ceci-gray-mid font-serif text-xs pl-1 md:pl-3 self-center">{data.ac.edges[0].node.frontmatter.date}</div></Link>
              </div>
                
              <div className="md:px-10 flex flex-col">
@@ -120,15 +120,15 @@ export default function FeatTrio() {
            
            <>
              <div className="flex flex-row">
-                 <div className="border-r-2 border-gray-600 uppercase tracking-wider text-ceci-gray-dark font-serif text-sm md:text-md pr-3"><Link to={`/exhibits`}>exhibits</Link></div>
+                <Link to={`/exhibits`}> <div className="border-r-2 border-gray-600 uppercase tracking-wider text-ceci-gray-dark font-serif text-sm md:text-md pr-3">exhibits</div></Link>
              </div>
             
-             <Link className="font-display tracking-wide uppercase py-2" to={`../exhibits/${ data.exhibit.edges[0].node.frontmatter.exhibit_slug }`}>{data.exhibit.edges[0].node.frontmatter.title }</Link>
-             <Link className="font-display tracking-wide uppercase py-2" to={`../exhibits/${ data.exhibit.edges[1].node.frontmatter.exhibit_slug }`}>{data.exhibit.edges[1].node.frontmatter.title }</Link>
-             <Link className="font-display tracking-wide uppercase py-2" to={`../exhibits/${ data.exhibit.edges[2].node.frontmatter.exhibit_slug }`}>{data.exhibit.edges[2].node.frontmatter.title }</Link>
-             <Link className="font-display tracking-wide uppercase py-2" to={`../exhibits/${ data.exhibit.edges[3].node.frontmatter.exhibit_slug }`}>{data.exhibit.edges[3].node.frontmatter.title }</Link>
-           <div className="font-display uppercase text-xs tracking-wide py-2"><Link  to={`../exhibits`}><FaArrowAltCircleRight className="inline mr-4"/> see more exhibits</Link> 
-             </div>
+             <Link className="font-display tracking-wide uppercase p-2" to={`../exhibits/${ data.exhibit.edges[0].node.frontmatter.exhibit_slug }`}>{data.exhibit.edges[0].node.frontmatter.title }</Link>
+             <Link className="font-display tracking-wide uppercase p-2" to={`../exhibits/${ data.exhibit.edges[1].node.frontmatter.exhibit_slug }`}>{data.exhibit.edges[1].node.frontmatter.title }</Link>
+             <Link className="font-display tracking-wide uppercase p-2" to={`../exhibits/${ data.exhibit.edges[2].node.frontmatter.exhibit_slug }`}>{data.exhibit.edges[2].node.frontmatter.title }</Link>
+             <Link className="font-display tracking-wide uppercase p-2" to={`../exhibits/${ data.exhibit.edges[3].node.frontmatter.exhibit_slug }`}>{data.exhibit.edges[3].node.frontmatter.title }</Link>
+           <Link  to={`../exhibits`}><div className="font-display uppercase text-xs tracking-wide py-2"><FaArrowAltCircleRight className="inline mr-4"/> see more exhibits 
+             </div></Link>
            </>
            </article>
           
