5c5abbfa6e168655b2f45b359c2aaf7f20d54cff
pls4e <31322343+pls4e@users.noreply.github.com>
2022-06-03 10:53:28 -0400

    fix npm 8 errors

--- src/utils/algolia-queries.js
+++ src/utils/algolia-queries.js
@@ -29,6 +29,57 @@ const bookQuery = `{
   }
 }`
 
+const RotundaQuery = `{
+  rotundas: AllRotundaJson {
+    edges {
+      node {
+        id
+        RotID
+        Title
+        Subtitle
+        MainDescription {
+          html
+        }
+        EditorCredit
+      }
+        
+    }
+  }
+}`
+
+const PageQuery = `{
+  pages: AllMarkdownRemark {
+    edges {
+      node {
+        html
+        fields {
+          slug
+        }
+      }  
+    }
+  }
+}`
+
+function RotundaToAlgoliaRecord({ node: { id, RotID, Title, Subtitle, MainDescription, MainCollection, SubCollection, EditorCredit } }) {
+  return {
+    objectID: id,
+    RotID,
+    Title,
+    Subtitle,
+    MainDescription,
+    AuthorCredit: EditorCredit,
+    Subject: MainCollection,
+    Series: SubCollection
+  }
+}
+
+function PageToAlgoliaRecord({ node: { id, html, fields } }) {
+  return {
+    objectID: id,
+    html,
+    fields
+  }
+}
 function bookToAlgoliaRecord({ node: { id, BookID, Title, Subtitle, MainDescription, AuthorCredit, Subject, Series, List, DaysSincePublication, PublicationDate, ISBNs} }) {
   return {
     objectID: id,
@@ -52,6 +103,18 @@ const queries = [
     transformer: ({ data }) => data.books.edges.map(bookToAlgoliaRecord),
     indexName,
     settings: { attributesToSnippet: [`MainDescription:20`], searchableAttributes: ['Title', 'Subtitle', 'MainDescription', 'AuthorCredit', 'ISBNs'], attributesForFaceting: ['Series.name', 'Subject.name', 'List'], typoTolerance: 'min', minWordSizefor1Typo:5 },
+  },
+  {
+    query: RotundaQuery,
+    transformer: ({ data }) => data.rotundas.edges.map(RotundaToAlgoliaRecord),
+    indexName,
+    settings: { attributesToSnippet: [`MainDescription.html:20`], searchableAttributes: ['Title', 'Subtitle', 'MainDescription', 'AuthorCredit'], attributesForFaceting: ['MainCollection', 'SubCollection'], typoTolerance: 'min', minWordSizefor1Typo:5 },
+  },
+  {
+    query: PageQuery,
+    transformer: ({ data }) => data.pages.edges.map(PageToAlgoliaRecord),
+    indexName,
+    settings: { attributesToSnippet: [`html:20`], searchableAttributes: ['Title', 'html'], attributesForFaceting: ['Series.name', 'Subject.name', 'List'], typoTolerance: 'min', minWordSizefor1Typo:5 },
   },
 ]
 
