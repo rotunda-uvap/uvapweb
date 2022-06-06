93f06533f114385832419d87bf2e853a7075e561
pls4e <31322343+pls4e@users.noreply.github.com>
2022-06-06 15:10:16 -0400

    key

--- gatsby-config.js
+++ gatsby-config.js
@@ -303,7 +303,7 @@ module.exports = {
       resolve: `gatsby-plugin-algolia`,
       options: {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_API_KEY,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
         indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
         queries: require("./src/utils/algolia-queries")
       },
