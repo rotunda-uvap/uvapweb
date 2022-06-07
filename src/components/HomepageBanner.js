d53f8abf2b7b2ba55f07a45f2193c41b262f0a7f
pls4e <31322343+pls4e@users.noreply.github.com>
2022-06-07 16:26:05 -0400

    homepage image ratio

--- src/components/HomepageBanner.js
+++ src/components/HomepageBanner.js
@@ -27,17 +27,17 @@ const uvafullimage = getImage(data.uvalogofull)
    
        <div className="mx-auto grid gap-3 md:gap-4 grid-cols-2 lg:grid-cols-3   ">
        <div id="1" className="col-span-2 lg:col-span-3 md:w-full py-5 place-content-center"><div className="px-4 md:px-auto text-center pb-5"><GatsbyImage image={uvafullimage} alt="uva logo" width={400} height={119} className="homepageimage" loading="eager"/></div></div> 
-       <div id="2" className="col-span-1 flex flex-col justify-center place-content-center"><Link to={booklink}>
+       <div id="2" className="col-span-1 flex flex-col justify-center place-content-center pr-2"><Link to={booklink}>
          <div className=" mx-auto font-sans font-light justify-self-center text-xs md:text-lg tracking-wide text-ceci-gray-mid text-right uppercase">{author}</div>
          <div className=" font-display text-xl md:text-3xl font-light text-right tracking-wide uppercase">{book_title}</div>
 <div className="font-serif py-2 text-lg text-xl max-w-sm text-right text-ceci-gray-mid">{subtitle}</div>
         </Link> </div>
          
         <div id="3" className="col-span-1 flex flex-col justify-center items-center"><div className="self-center">
-        <Link to={booklink}> <img width={250} src={cover} alt="feat book" className="drop-shadow-img homepageimage" loading="eager" /></Link></div>
+        <Link to={booklink}> <img src={cover} alt="feat book" className="aspect-[5/8] drop-shadow-img homepageimage" loading="eager" /></Link></div>
           </div>
         
-          <div id="4" className="hidden lg:block lg:col-span-1 py-3 font-serif font-light text-2xl quoted self-center text-left">
+          <div id="4" className="hidden lg:block lg:col-span-1 py-3 font-serif font-light text-2xl quoted self-center text-left md:pl-2">
           <Link to={booklink}>{quote}{attr && <div className="self-center text-ceci-gray-mid italic text-lg">- { attr}</div>}</Link></div>
         
       </div>
