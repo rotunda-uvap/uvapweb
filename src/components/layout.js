7ed7661f8c315615795fbd65d92864a4153d9521
pls4e <31322343+pls4e@users.noreply.github.com>
2022-06-11 13:45:07 -0400

    gtm in script

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,8 @@ import React from "react"
 import Footer from "./footer" 
 import MegaHeader from "./Megamenu"
 import { Helmet } from "react-helmet"
+import { Script } from "gatsby"
+
 
 const Layout = ({ children }) => {
  
@@ -10,8 +12,25 @@ const Layout = ({ children }) => {
     <Helmet
   htmlAttributes={{
     lang: 'en',
-  }}
+  }}/>
+  <Script
+  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTM}`}
+  strategy="off-main-thread"
+  forward={[`gtag`]}
 />
+<Script id="gtag-config" strategy="off-main-thread">
+  {`
+    window.dataLayer = window.dataLayer || []
+    window.gtag = function gtag() { window.dataLayer.push(arguments) }
+    gtag('js', new Date())
+    gtag('config', ${process.env.GTM}, { send_page_view: false })
+  `}
+</Script>
+   
+
+  
+
+  
     <div className="max-w-full mx-auto px-5 py-5">
     
       <MegaHeader/>
