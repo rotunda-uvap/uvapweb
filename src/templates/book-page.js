ae964da026c7e8c6e99f03080d34679f0411fe0e
pls4e <31322343+pls4e@users.noreply.github.com>
2022-07-06 10:21:45 -0400

    svg swap

--- src/templates/book-page.js
+++ src/templates/book-page.js
@@ -170,20 +170,20 @@ const bookPage = ({ data }) => {
           ))}
 
           <div className="flex flex-row inline-flex items-center w-full leading-normal">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-ceci-gray-mid"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-              />
-            </svg>
+          <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-ceci-gray-mid"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+                />
+              </svg>
             <a
               href={`https://forms.gle/H8J1NbtN4BBZZwgy7`}
               className="block group  p-2 ml-4 border-b"
