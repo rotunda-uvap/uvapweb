d38dd2a9fe7881b67237261a6e62b5d9fa49c37a
pls4e <31322343+pls4e@users.noreply.github.com>
2022-11-29 19:56:41 -0500

    remove trailing slash config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@
 const path = require(`path`)
 module.exports = {
   flags: { PARALLEL_SOURCING: true, FAST_DEV: true , DEV_SSR: false},
-  trailingSlash: `ignore`,
   siteMetadata: {
     title: `University of Virginia Press`,
     titleTemplate: "%s - UVA Press",
