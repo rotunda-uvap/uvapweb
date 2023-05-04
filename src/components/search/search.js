1261c6bd4c207fead89924223d92ecd3a39722af
pls4e <31322343+pls4e@users.noreply.github.com>
2023-05-04 11:45:09 -0400

    filter

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -26,7 +26,13 @@ const Search = () => (
       <div className="flex flex-col md:flex-row gap-8">
      
       <section className="col-span-2" id="results">
-      <a href="#filter" className="md:hidden py-6 text-sm tracking-wide font-thin">Filter Results</a>
+        
+      <a href="#filter" className="md-hidden flex flex-row gap-2 py-8 text-sm tracking-wide font-thin">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+</svg><span>Filter Results</span>
+      </a>
+      
         <h3 className="text-lg uppercase text-ceci-gray-mid py-2 font-thin tracking-widest">Results
 </h3>
 
