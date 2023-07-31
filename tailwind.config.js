4fff19ed864ee5b5bb1a57a58988652a342e1944
pls4e <31322343+pls4e@users.noreply.github.com>
2023-07-31 17:38:18 -0400

    module-exports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}','node_modules/@algolia/*' ],
   theme: {
     letterSpacing: {
       tightest: '-.075em',
