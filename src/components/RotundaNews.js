4b498b7f3f758dd8352c0b151d5e165043b55282
pls4e <31322343+pls4e@users.noreply.github.com>
2023-09-18 11:25:29 -0400

    olmsted

--- src/components/RotundaNews.js
+++ src/components/RotundaNews.js
@@ -6,7 +6,7 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 export default function RotundaNews() { 
   const data = useStaticQuery(graphql`
   query {
-    image: file(relativePath: { eq: "arch_sm.png" }) {
+    image: file(relativePath: { eq: "OLMS.jpg" }) {
       childImageSharp {
         gatsbyImageData(
           placeholder: BLURRED
@@ -29,13 +29,13 @@ export default function RotundaNews() {
               <div className="flex flex-row md:px-10 "> 
               
                   <div className="border-r-2 font-serif uppercase tracking-wider text-sm self-center text-ceci-gray-dark font-serif text-md pr-3">rotunda news</div>
-                  <div className=" text-ceci-gray-mid font-serif text-sm pl-3 pt-1">2023-06-13</div>
+                  <div className=" text-ceci-gray-mid font-serif text-sm pl-3 pt-1">2023-08-13</div>
               </div>
                 
               <div className="md:px-10 flex flex-col">
-              <Link className="font-display tracking-wide uppercase py-2 font-thin"  to={`https://www.sah.org/about-sah/news/news-detail/2023/07/24/celebrating-10-years-of-sah-archipedia`}>SAH Archipedia Turns Ten!</Link>
-            <div><p className="font-serif text-ceci-gray-dark leading-relaxed dropCap">A decade ago, the Society of Architectural Historians and University of Virginia Press first published <a href="https://sah-archipedia.org/">SAH Archipedia</a>, the authoritative online architectural encyclopedia of the U.S. built environment. Since then, over a million visitors have enjoyed the essays, entries, and images of sites all over the country—from prehistoric to contemporary, vernacular to high style, landscapes to urban settings.</p> </div>  
-            <div className="font-display uppercase text-xs tracking-widest"><Link className="font-display tracking-wide uppercase py-2" to={`https://www.sah.org/about-sah/news/news-detail/2023/07/24/celebrating-10-years-of-sah-archipedia`}><FaArrowAltCircleRight className="inline mx-4"/>read full article at SAH.org</Link> </div></div>
+              <Link className="font-display tracking-wide uppercase py-2 font-thin"  to={`https://olmsted.org/open-access-the-papers-of-frederick-law-olmsted/`}>The Papers of Frederick Law Olmsted Go Open Access</Link>
+            <div><p className="font-serif text-ceci-gray-dark leading-relaxed dropCap">Thanks to generous subvention from the Olmsted Network, the <a href="https://rotunda.upress.virginia.edu/founders/OLMS.html">Papers of Frederick Law Olmsted</a> are now freely available to all!</p> </div>  
+            <div className="font-display uppercase text-xs tracking-widest"><Link className="font-display tracking-wide uppercase py-2" to={`https://olmsted.org/open-access-the-papers-of-frederick-law-olmsted/`}><FaArrowAltCircleRight className="inline mx-4"/>read full press release at the Olmsted Network</Link> </div></div>
          
             
             </>
