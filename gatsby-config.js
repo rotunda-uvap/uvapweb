5a1dd94d5617757ccfa9bef4a5e74110e3f55424
pls4e <31322343+pls4e@users.noreply.github.com>
2024-07-26 15:43:04 -0400

    adding back netlify-cms plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -183,6 +183,14 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-netlify-cms-paths`,
+      options: {
+        // Path to your Netlify CMS config file
+        cmsConfig: `/static/admin/config.yml`,
+        // stylesPath: `${__dirname}/src/utils/global.css`
+      }
+    },
+    {
       resolve: 'gatsby-plugin-static-folders',
       options: {
         folders: [
