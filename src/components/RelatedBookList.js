15b48e189e1b62b8dc3118d45603466a6c3259d8
patricia searl <pls4e@virginia.edu>
2026-03-25 15:36:46 -0400

    book no cover fix

--- src/components/RelatedBookList.js
+++ src/components/RelatedBookList.js
@@ -1,29 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 const RelatedBookList = ({ id, title }) => {
    const cover = `https://ik.imagekit.io/uvapress/${id}_M.jpg`
    const bookLink = `/title/${id}`
+   const [imgFailed, setImgFailed] = useState(false)
 
    return (
       <Link to={bookLink} className="object-cover px-2">
-         <img src={cover} alt={`Cover of ${title}`} title={title} />
+         {imgFailed ? (
+            <div style={{ width: 150, height: 224, display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "white", border: "1px solid #e5e7eb" }}>
+               <p className="text-base text-ceci-gray-dark uppercase pt-3 px-2 underline font-sans text-center">{title}</p>
+            </div>
+         ) : (
+            <img
+               src={cover}
+               alt={`Cover of ${title}`}
+               title={title}
+               width="150"
+               height="224"
+               style={{ objectFit: "contain" }}
+               onError={() => setImgFailed(true)}
+            />
+         )}
       </Link>
    )
 }
 
 export default RelatedBookList
-/* import React from "react"
-import { Link } from "gatsby"
-
-const RelatedBookList = ({id, title}) => {
-   const cover = "https://ik.imagekit.io/uvapress/" + id + "_M.jpg"
-   const booklink = "../../title/" + id
-
-    return (
-            <Link to={booklink} className="object-cover px-2"><img src={cover} alt={title} title={title}/></Link>
-  
-      )
-  }
-  
-  export default RelatedBookList */
